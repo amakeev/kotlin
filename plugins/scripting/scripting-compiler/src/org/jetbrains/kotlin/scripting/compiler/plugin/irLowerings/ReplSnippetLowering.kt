0e2e1a8fd262232d9444667a6351164230094016
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-04-30 14:48:28 +0200

    [IR] Avoid insert/removeReceiver methods needed only for old param API
    Now that we migrated the compiler code to the new parameter API we can
    work on the arguments list directly.

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/irLowerings/ReplSnippetLowering.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/irLowerings/ReplSnippetLowering.kt
@@ -509,7 +509,7 @@ private class ReplSnippetToClassTransformer(
     override fun visitConstructorCall(
         expression: IrConstructorCall,
         data: ScriptLikeToClassTransformerContext,
-    ): IrExpression {
+    ): IrElement {
         return if ((expression.symbol.owner.parent as? IrDeclaration)?.let { it in irSnippet.declarationsFromOtherSnippets } == true) {
             expression.arguments +=
                 accessCallsGenerator.createAccessToSnippet(
