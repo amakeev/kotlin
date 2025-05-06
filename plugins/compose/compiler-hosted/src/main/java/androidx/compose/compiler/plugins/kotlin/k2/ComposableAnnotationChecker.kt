880405f898f4fd649bee758e5d06a39e41793da8
Andrei Shikov <ashikov@google.com>
2025-05-06 15:00:59 +0100

    Account for vararg copying annotations from arguments in ComposableAnnotationChecker
    vararg arguments are presented as `@Composable Array<@Composable () -> Unit>`, which was tripping the checker.
    
    ^KT-77219

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/k2/ComposableAnnotationChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/k2/ComposableAnnotationChecker.kt
@@ -6,6 +6,7 @@
 package androidx.compose.compiler.plugins.kotlin.k2
 
 import androidx.compose.compiler.plugins.kotlin.ComposeClassIds
+import org.jetbrains.kotlin.KtFakeSourceElementKind
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
@@ -14,12 +15,21 @@ import org.jetbrains.kotlin.fir.analysis.checkers.type.FirResolvedTypeRefChecker
 import org.jetbrains.kotlin.fir.declarations.getAnnotationByClassId
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
 import org.jetbrains.kotlin.fir.types.functionTypeKind
+import org.jetbrains.kotlin.fir.types.isNonPrimitiveArray
 
 object ComposableAnnotationChecker : FirResolvedTypeRefChecker(MppCheckerKind.Common) {
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(typeRef: FirResolvedTypeRef) {
         val composableAnnotation = typeRef.getAnnotationByClassId(ComposeClassIds.Composable, context.session)
         if (composableAnnotation == null) {
+            return
+        }
+
+        // ignore if the type is coming from vararg
+        if (
+            typeRef.delegatedTypeRef?.source?.kind == KtFakeSourceElementKind.ArrayTypeFromVarargParameter &&
+            typeRef.coneType.isNonPrimitiveArray
+        ) {
             return
         }
 
